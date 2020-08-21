e68a1160c4951728b79389ef5a0a6104c2919fa9
sireesha <sirisharao485@gmail.com>
2020-08-21 15:45:43 +0530

    initial setup

---
+++ src/main/java/com/orderapplication/OrderapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.orderapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderapplicationApplication.class, args);
+	}
+
+}
